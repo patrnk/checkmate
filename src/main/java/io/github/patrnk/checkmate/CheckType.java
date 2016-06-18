2ff709ffc87cbdc6642dd78fe6afe70520409aa5
Paul Vergeev <paulvergeev@gmail.com>
2016-06-18 10:42:53 +0300

    Add toString() implementation.

--- src/main/java/io/github/patrnk/checkmate/CheckType.java
+++ src/main/java/io/github/patrnk/checkmate/CheckType.java
@@ -2,4 +2,23 @@ package io.github.patrnk.checkmate;
 
 public enum CheckType {
     SIMPLE, STANDART, ADVANCED;
+    
+    /**
+     * Gets a name of corresponding GeneralTest subclass.
+     * @return the exact name of the class
+     * @throws UnsupportedOperationException if no name is provided 
+     */
+    @Override
+    public String toString() throws UnsupportedOperationException {
+        switch (this) {
+//            case SIMPLE:
+//                return "Simple";
+//            case STANDART:
+//                return "Standart";
+//            case ADVANCED:
+//                return "Advanced";
+            default: 
+                throw new UnsupportedOperationException("Add name of ");
+        }
+    } 
 }
