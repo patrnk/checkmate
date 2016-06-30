bd717925e2a9e5879bdd88b1ff754ab3a32c0434
Paul Vergeev <paulvergeev@gmail.com>
2016-06-30 19:19:26 +0300

    Implement Singleton pattern.

--- src/main/java/io/github/patrnk/checkmate/CheckerWithATwist.java
+++ src/main/java/io/github/patrnk/checkmate/CheckerWithATwist.java
@@ -2,9 +2,23 @@ package io.github.patrnk.checkmate;
 
 import java.util.Map;
 
+/**
+ * It's a singleton.
+ * @author vergeev
+ */
+public final class CheckerWithATwist implements TestChecker {
 
-public class CheckerWithATwist implements TestChecker {
-
+    private static CheckerWithATwist instance = null;
+    
+    private CheckerWithATwist() {}
+    
+    public static CheckerWithATwist getInstance() {
+        if (instance == null) {
+            instance = new CheckerWithATwist();
+        }
+        return instance;
+    }
+    
     @Override
     public TestResult check(Map<Integer, String> studentAnswers) {
         throw new UnsupportedOperationException("Not supported yet.");
