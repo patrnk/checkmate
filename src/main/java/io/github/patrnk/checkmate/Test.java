fdb78d29932e7d1a1c857232dbf0757cb01e5de8
Paul Vergeev <paulvergeev@gmail.com>
2016-07-03 19:22:55 +0300

    Add JavaDoc to check() method.

--- src/main/java/io/github/patrnk/checkmate/Test.java
+++ src/main/java/io/github/patrnk/checkmate/Test.java
@@ -8,5 +8,10 @@ import java.util.List;
  * @author vergeev
  */
 public interface Test extends Serializable {
+    /**
+     * Grades each student answer individually.
+     * @param studentAnswers list of answers where i-th element is (i+1)-th answer.
+     * @return same <code>studentAnswers</code> but with grades
+     */
     public List<TestAnswer> check(List<TestAnswer> studentAnswers);
 }
