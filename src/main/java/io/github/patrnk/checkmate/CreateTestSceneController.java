770bafdce6d2a6abb4e87f2ab101ec7dab2f64f6
Paul Vergeev <paulvergeev@gmail.com>
2016-07-05 18:07:55 +0300

    Add factoryBox initializer.

--- src/main/java/io/github/patrnk/checkmate/CreateTestSceneController.java
+++ src/main/java/io/github/patrnk/checkmate/CreateTestSceneController.java
@@ -6,13 +6,18 @@
 package io.github.patrnk.checkmate;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -20,28 +25,58 @@ import javafx.scene.control.TextArea;
  * @author vergeev
  */
 public class CreateTestSceneController implements Initializable {
-
-    @FXML
-    private TextArea contentArea;
-    
-    @FXML
-    private void saveButtonClicked(ActionEvent event) {
-    }
     
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        addCheckFactories();
-    }    
+        initializeCheckerFactories();
+        factoryBox.setItems(getFactoryTitles(checkerFactories));
+    }
     
-    List<TestFactory> checkerFactories = new ArrayList();
+    ObservableList<TestFactory> checkerFactories = FXCollections.observableArrayList();
     
-    /**
-     * Initializes checker factories list. 
-     */
-    private void addCheckFactories() {
+    private void initializeCheckerFactories() {
         checkerFactories.add(new PermissiveTestFactory());
+    }
+    
+    private ObservableList<String> getFactoryTitles(ObservableList<TestFactory> factories) {
+        ObservableList<String> titles = FXCollections.observableArrayList();
+        for (TestFactory factory : factories) {
+            titles.add(factory.getTitle());
+        }
+        return titles;
+    }
+    
+    @FXML
+    private TextField nameField;
+    
+    @FXML
+    private TextField idField;
+    
+    @FXML
+    private TextArea contentArea;
+    
+    @FXML
+    private ComboBox<String> factoryBox;
+    
+    @FXML
+    private Label testSummaryLabel;
+    
+    @FXML
+    private Label errorLabel;
+    
+    @FXML
+    private void saveButtonClicked(ActionEvent event) {
+        TestInfo info = new TestInfo(nameField.getText(), 
+            Long.valueOf(idField.getText()), contentArea.getText());
+        try {
+            Test test = new PermissiveTest(info);
+        } catch (MalformedTestDescriptionException ex) {
+            Logger.getLogger(CreateTestSceneController.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (AnswerNotProvidedException ex) {
+            Logger.getLogger(CreateTestSceneController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
