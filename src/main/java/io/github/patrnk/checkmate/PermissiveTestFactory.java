0b698dca829af02f24539e0604d327f35da3feab
Paul Vergeev <paulvergeev@gmail.com>
2016-07-06 18:03:19 +0300

    Make PermissiveTestFactory final.

--- src/main/java/io/github/patrnk/checkmate/PermissiveTestFactory.java
+++ src/main/java/io/github/patrnk/checkmate/PermissiveTestFactory.java
@@ -1,7 +1,7 @@
 package io.github.patrnk.checkmate;
 
 
-public class PermissiveTestFactory implements TestFactory {
+public final class PermissiveTestFactory implements TestFactory {
     private static final String TITLE = "Нестандартный";
     private static final String SUMMARY = "...";
     
