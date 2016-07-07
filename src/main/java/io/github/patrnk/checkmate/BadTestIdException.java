636cab1392d2db2d3b5bb752b6e1926ad9bb70b9
Paul Vergeev <paulvergeev@gmail.com>
2016-07-07 11:38:04 +0300

    Add BadTestIdException exception.

---
+++ src/main/java/io/github/patrnk/checkmate/BadTestIdException.java
@@ -0,0 +1,12 @@
+package io.github.patrnk.checkmate;
+
+
+public class BadTestIdException extends Exception {
+    public BadTestIdException() {
+        super();
+    }
+    
+    public BadTestIdException(String message) {
+        super(message);
+    }
+}
