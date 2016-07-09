06f7929e92f109f5128b50d4ad8110c145e34b59
Paul Vergeev <paulvergeev@gmail.com>
2016-07-09 11:00:08 +0300

    Make PersistenceManager a utility class that's impossible to instantiate.

--- src/main/java/io/github/patrnk/checkmate/PersistenceManager.java
+++ src/main/java/io/github/patrnk/checkmate/PersistenceManager.java
@@ -7,14 +7,14 @@ import java.io.ObjectOutputStream;
 
 public final class PersistenceManager {
     
-    private final String TESTS_FOLDER = "tests";
+    private final static String TESTS_FOLDER = "tests";
     
     /**
      * Persists test over time serializing it into a file.
      * @param t test to serialize
      * @throws BadTestIdException if the test with that id already exists. 
      */
-    public void writeDownTest(Test t) throws BadTestIdException {
+    public static void writeDownTest(Test t) throws BadTestIdException {
         String filename = t.getInfo().getId().toString();
         if (alreadyExists(TESTS_FOLDER, filename)) {
             throw new BadTestIdException("A test with id " + filename + 
@@ -23,7 +23,7 @@ public final class PersistenceManager {
         writeDown(TESTS_FOLDER, filename, t);
     }
     
-    private Boolean alreadyExists(String folderPath, String name) {
+    private static Boolean alreadyExists(String folderPath, String name) {
         File folder = new File(folderPath);
         File[] files = folder.listFiles();
         for (File file : files) {
@@ -36,7 +36,7 @@ public final class PersistenceManager {
         return false;
     }
     
-    private void writeDown(String folderPath, String filename, Object o) {
+    private static void writeDown(String folderPath, String filename, Object o) {
         try {
             String filepath = folderPath + File.separator + filename;
             FileOutputStream out = new FileOutputStream(filepath);
@@ -46,5 +46,9 @@ public final class PersistenceManager {
         } catch (Exception e) {
             CmUtils.printException(e);
         }
+    }
+    
+    private PersistenceManager() {
+        throw new AssertionError("You cannot instantiate PersistenceManager");
     }
 }
