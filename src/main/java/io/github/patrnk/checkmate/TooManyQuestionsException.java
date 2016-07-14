b8296b78a21ab6911b398549abc2a92bcb5a3306
Paul Vergeev <paulvergeev@gmail.com>
2016-07-14 15:15:11 +0300

    Add TooManyQuestionsException class.

---
+++ src/main/java/io/github/patrnk/checkmate/TooManyQuestionsException.java
@@ -0,0 +1,20 @@
+package io.github.patrnk.checkmate;
+
+
+public class TooManyQuestionsException extends Exception {
+    public TooManyQuestionsException() {
+        super();
+    }
+    
+    public TooManyQuestionsException(String message) {
+        super(message);
+    }
+    
+    public TooManyQuestionsException(Throwable cause) {
+        super(cause);
+    }
+
+    public TooManyQuestionsException(String message, Throwable cause) {
+        super(message, cause);
+    }  
+}
