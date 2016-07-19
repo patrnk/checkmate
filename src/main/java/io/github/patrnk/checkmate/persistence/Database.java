0d8566448fd1168b3551b0c240640dc20753338d
Paul Vergeev <paulvergeev@gmail.com>
2016-07-19 18:01:53 +0300

    Fix broken SQL request and connection initialization.

--- src/main/java/io/github/patrnk/checkmate/persistence/Database.java
+++ src/main/java/io/github/patrnk/checkmate/persistence/Database.java
@@ -20,12 +20,14 @@ final class Database {
     private static final Integer MAX_STRING_LENGTH = 255;
     
     private static Connection getConnection() {
-        try {
-            Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite:mockup.db");
-            createTablesIfNeeded();
-        } catch (ClassNotFoundException | SQLException ex) {
-            CmUtils.printExceptionAndExit(ex);
+        if (connection == null) {
+            try {
+                Class.forName("org.sqlite.JDBC");
+                connection = DriverManager.getConnection("jdbc:sqlite:mockup.db");
+                createTablesIfNeeded();
+            } catch (ClassNotFoundException | SQLException ex) {
+                CmUtils.printExceptionAndExit(ex);
+            }
         }
         return connection;
     }
@@ -40,9 +42,9 @@ final class Database {
         if (!tables.next()) {   
             String createQuery = "CREATE TABLE testResult " +
                 "(test_id int, " +
-                "student_name nvarchar(" + MAX_STRING_LENGTH.toString() + ")," +
-                "student_id nvarchar(" + MAX_STRING_LENGTH.toString() + "), " +
-                "answer_file nvarchar(" + MAX_STRING_LENGTH.toString() + "))";
+                "student_name varchar(" + MAX_STRING_LENGTH.toString() + ")," +
+                "student_id varchar(" + MAX_STRING_LENGTH.toString() + "), " +
+                "answer_file varchar(" + MAX_STRING_LENGTH.toString() + "))";
             try (Statement createTable = connection.createStatement()) {
                 createTable.executeUpdate(createQuery);
             }
@@ -61,44 +63,51 @@ final class Database {
     public static void addRecord(Record record) throws SQLException {
         checkStringLength(record.getStudentName(), record.getStudentId(), 
             record.getAnswerFileName());
-        
         String insertSql = "INSERT INTO testResult " +
                 "(test_id, student_name, student_id, answer_file) VALUES " +
-                "(      ?,            ?,          ?,           ?)";
+                "(      ?,            ?,          ?,           ?);";
         PreparedStatement insert = 
             Database.getConnection().prepareStatement(insertSql);
-        
-        insert.setInt(1, record.getTestId());
-        insert.setString(2, record.getStudentName());
-        insert.setString(3, record.getStudentId());
-        insert.setString(4, record.getAnswerFileName());
-        insert.executeUpdate();
+        try {
+            insert.setInt(1, record.getTestId());
+            insert.setString(2, record.getStudentName());
+            insert.setString(3, record.getStudentId());
+            insert.setString(4, record.getAnswerFileName());
+            insert.executeUpdate();
+        } finally {
+            insert.close();
+        }
     }
     
     public static List<Record> fetchRecords() throws SQLException {
         List<Record> records = new ArrayList();
-        String selectSql = "SELECT * FROM testResult";
+        String selectSql = "SELECT * FROM testResult;";
         Statement select = Database.getConnection().createStatement();
-        ResultSet rawRecords = select.executeQuery(selectSql);
-        while (rawRecords.next()) {
-            Integer testId = rawRecords.getInt("test_id");
-            String studentName = rawRecords.getNString("student_name");
-            String studentId = rawRecords.getNString("student_id");
-            String answerFilename = rawRecords.getNString("answer_file");
-            try {
-                Record record = new Record(testId, studentName, studentId, answerFilename);
-                records.add(record);
-            } catch (BadStudentNameException | BadStudentIdException ex) {
-                // Is this how you log the thing? 
-                Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        try { 
+            ResultSet rawRecords = select.executeQuery(selectSql);
+            while (rawRecords.next()) {
+                Integer testId = rawRecords.getInt("test_id");
+                String studentName = rawRecords.getString("student_name");
+                System.out.println(rawRecords.getString("student_name"));
+                String studentId = rawRecords.getString("student_id");
+                String answerFilename = rawRecords.getString("answer_file");
+                try {
+                    Record record = new Record(testId, studentName, studentId, answerFilename);
+                    records.add(record);
+                } catch (BadStudentNameException | BadStudentIdException ex) {
+                    // Is this how you log the thing? 
+                    Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+                }
             }
+        } finally {
+            select.close();
         }
         return records;
     }
      
     private static void checkStringLength(String... strings) {
         for (String s : strings) {
-            if (s.length() > MAX_STRING_LENGTH) { // TODO: get rid of 255
+            if (s.length() > MAX_STRING_LENGTH) {
                 throw new IllegalArgumentException("Every string in "
                     + " the database must be " + MAX_STRING_LENGTH + 
                     " symbols long of shorter.");
