0f022fa6698685f2f58bb2df87e5673e47963c78
patrnk <patrnk@gmail.com>
2016-07-27 17:32:58 +0400

    Remove printExceptionAndExit as it's never used.

--- src/main/java/io/github/patrnk/checkmate/CmUtils.java
+++
@@ -1,9 +0,0 @@
-package io.github.patrnk.checkmate;
-
-
-public class CmUtils {
-    public static void printExceptionAndExit(Exception e) {
-        System.err.println(e.getClass().getName() + ": " + e.getMessage());
-        System.exit(0);
-    }
-}
