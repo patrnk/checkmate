2319d759fc7c36f799e344c502b1400df02afdcf
Paul Vergeev <paulvergeev@gmail.com>
2016-08-14 13:19:34 +0300

    Replace get...FileName() methods with get...FilePath()
    Since we only allow certain extensions in certain folders, it's logical
    to tightly couple the filename formatting function and 
    folder path formatting one.

--- src/main/java/io/github/patrnk/checkmate/persistence/PathManager.java
+++ src/main/java/io/github/patrnk/checkmate/persistence/PathManager.java
@@ -12,7 +12,6 @@ class PathManager {
     
     /**  
      * Returns the folder path where you can store Test objects.
-     * The returned string has File.separator as last character for convenience.
      * Ensures that the directory actually exists.
      * @throws IOException if the directory doesn't exist and cannot be created.
      */
@@ -23,12 +22,11 @@ class PathManager {
     }
     
     private String constructTestFolderPath() {
-        return COMMON_FOLDER + File.separator + TEST_FOLDER + File.separator;
+        return COMMON_FOLDER + File.separator + TEST_FOLDER;
     }
     
     /**
      * Returns the folder path where you can store test results.
-     * The returned string has File.separator as last character for convenience.
      * Ensures that the directory actually exists.
      * @throws IOException if the directory doesn't exist and cannot be created.
      */
@@ -39,7 +37,7 @@ class PathManager {
     }
     
     private String constructResultFolderPath() {
-        return COMMON_FOLDER + File.separator + RESULT_FOLDER + File.separator;
+        return COMMON_FOLDER + File.separator + RESULT_FOLDER;
     }
     
     private void ensureFileExists(String directory) throws IOException {
@@ -55,12 +53,36 @@ class PathManager {
     private final String RESULT_SUFFIX = "res";
     
     /**
+     * Formats the name of the file and concatenates it to the folder path.
+     * @param filename the name of the file that's intended for Test object.
+     * @return path which can be used to create the file.
+     * @throws IOException if the folder of the file doesn't exist 
+     *      and cannot be created.
+     */
+    public String getTestFilePath(String filename) throws IOException {
+        String formattedName = this.getTestFileName(filename);
+        return this.getTestFolderPath() + File.separator + formattedName;
+    }
+    
+    /**
      * Formats the name of the file that's intended for Test object. 
      * @param name the name to format.
      * @return the formatted name.
      */
-    public String getTestFileName(String name) {
+    private String getTestFileName(String name) {
         return name + SUFFIX_SEPARATOR + TEST_SUFFIX;
+    }
+    
+    /**
+     * Formats the name of the file and concatenates it to the folder path.
+     * @param filename the name of the file that's intended for test results.
+     * @return path which can be used to create the file.
+     * @throws IOException if the folder of the file doesn't exist 
+     *      and cannot be created.
+     */
+    public String getResultFilePath(String filename) throws IOException {
+        String formattedName = this.getResultFileName(filename);
+        return this.getResultFolderPath() + File.separator + formattedName;
     }
     
     /**
@@ -68,7 +90,7 @@ class PathManager {
      * @param name the name to format.
      * @return the formatted name.
      */
-    public String getResultFileName(String name) {
+    private String getResultFileName(String name) {
         return name + SUFFIX_SEPARATOR + RESULT_SUFFIX;
     }
 }
