38571e1740e6a3266d3abcd48ef3d422a14c9a90
Paul Vergeev <paulvergeev@gmail.com>
2016-08-15 15:12:43 +0300

    Add documentation

--- src/main/java/io/github/patrnk/checkmate/persistence/Record.java
+++ src/main/java/io/github/patrnk/checkmate/persistence/Record.java
@@ -1,28 +1,42 @@
 package io.github.patrnk.checkmate.persistence;
 
-
+/**
+ * Entity that contains information about stored test results.
+ */
 public final class Record {
     
     private final Integer testId;
     
+    /**
+     * Returns id of the test to which the results belong to.
+     */
     public Integer getTestId() {
         return testId;
     }
     
     private final String studentName;
     
+    /**
+     * Returns name of the student the result belongs to. 
+     */
     public String getStudentName() {
         return studentName;
     }
     
     private final String studentId;
     
+    /**
+     * Returns id of the student the result belongs to.
+     */
     public String getStudentId() {
         return studentId;
     }
      
     private final String resultFilepath;
     
+    /**
+     * Returns path to the file where the result is stored.
+     */
     public String getResultFilepath() {
         return resultFilepath;
     }
