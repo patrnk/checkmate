dfc743e8a2e3f9bee8fd42d93b775e25ec2bacd9
Paul Vergeev <paulvergeev@gmail.com>
2016-08-16 11:54:26 +0300

    Add summary

--- src/main/java/io/github/patrnk/checkmate/test/PermissiveTestFactory.java
+++ src/main/java/io/github/patrnk/checkmate/test/PermissiveTestFactory.java
@@ -6,7 +6,10 @@ import io.github.patrnk.checkmate.test.exception.BadTestInfoException;
 public final class PermissiveTestFactory implements TestFactory {
     
     private static final String TITLE = "Нестандартный";
-    private static final String SUMMARY = "...";
+    private static final String SUMMARY = "Каждый полный правильный ответ "
+        + "стоит 2 балла. Если учащийся отвечает на вопрос без ошибки, но не "
+        + "все верные ответы находит, то ему присуждается 1 балл. Если же он "
+        + "делает ошибку в ответе, то этот вопрос стоит 0 баллов.";
     
     @Override
     public String getTitle() {
