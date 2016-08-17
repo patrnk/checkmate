c5482eae71f9b90fac38775ab997692bf32a416a
Paul Vergeev <paulvergeev@gmail.com>
2016-08-17 19:52:03 +0300

    Add JavaDoc

--- src/main/java/io/github/patrnk/checkmate/test/TestFactory.java
+++ src/main/java/io/github/patrnk/checkmate/test/TestFactory.java
@@ -4,8 +4,16 @@ import io.github.patrnk.checkmate.test.exception.BadTestInfoException;
 
 public interface TestFactory {
     
+    /**
+     * Returns the title of the category of tests.
+     * It's used to distinguish between different ones.
+     */
     public String getTitle();
-    
+
+    /**
+     * Returns the summary of the category of tests.
+     * Summary describes how student answers are graded.
+     */    
     public String getSummary();
     
     public Test getTest(TestInfo test) throws BadTestInfoException;
