254652d5add3dedbd62df3c87f824aa86d656db1
Paul Vergeev <paulvergeev@gmail.com>
2016-08-18 12:46:26 +0300

    Update Javadoc

--- src/main/java/io/github/patrnk/checkmate/test/Test.java
+++ src/main/java/io/github/patrnk/checkmate/test/Test.java
@@ -9,12 +9,15 @@ public interface Test extends Serializable {
     
     /**
      * Grades each student answer individually.
-     * Ignores an answers if there's no key for it.
+     * Ignores an answer if there's no key for it.
      * @param studentAnswers list of answers where i-th element is (i+1)-th answer.
      * @return list of TestAnswer with the same answers and grades. All answers
      *      with number greater than that provided in answer key are dropped.
      */
     public ArrayList<TestAnswer> check(ArrayList<TestAnswer> studentAnswers);
     
+    /**
+     * Get max possible amount of points a student can get for the test.
+     */
     public Integer getMaxGrade();
 }
